7ca628fe45224c41fc4d4c831849619ce5bcc6a6
JelleS-immalle <jelle.swaelen@immalle.be>
2017-03-06 17:12:03 +0100

    Niet volledig werkende oplossing toegevoegd voor vraag 03

--- 03_prepared.php
+++ 03_prepared.php
@@ -9,6 +9,23 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $stmt = $conn->prepare("INSERT INTO messages(inhoud, tijdstip) VALUES(:inhoud, now())");
+    
+    //Variabele gebonden aan de inhoud van :inhoud
+    $stmt->bindParam(":inhoud", $inhoudBericht);
+
+    //Eerste bericht
+    $inhoudBericht = 'Hey';
+    $stmt->exec();
+
+    //Tweede bericht
+    $inhoudBericht = 'ik ben';
+    $stmt->exec();
+
+
+    //Derde bericht
+    $inhoudBericht = 'een test bericht.';
+    $stmt->execute();
+
 
     // vervolledig de code om 3 testberichten toe te voegen
     // m.b.v. bovenstaand prepared statement
